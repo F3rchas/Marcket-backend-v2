50a75edb488359ca0c1be0cd20f61fea1d9926ec
Fenando Rueda <git@github.com:F3rchas/market-backend.git>
2026-09-08 17:32:45 -0600

    First commit

---
+++ src/main/java/com/merida/tecnm/Marcket_backend_v2/MarcketBackendV2Application.java
@@ -0,0 +1,13 @@
+package com.merida.tecnm.Marcket_backend_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcketBackendV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcketBackendV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/merida/tecnm/Marcket_backend_v2/MarcketBackendV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merida.tecnm.Marcket_backend_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarcketBackendV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
